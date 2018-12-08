2699e4ee8ade831cc60ed66dede7a65fe75f03f9
kiq005 <kiq005@gmail.com>
2018-12-08 12:44:59 -0200

    corrigido método de obtenção dos valores

--- src/csv_to_json.py
+++ src/csv_to_json.py
@@ -1,62 +1,81 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import re, json, os
+import re, csv, json, os
 
 DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'dataset')
-CSV_REGEX = r'(\d+),"?(\[[^\]]+\]|\[\])"?,([^,]+)?,(\d+),"?(\[[^\]]+\]|\[\])"?,(\w+),"?([^\"]+)"?,"?([^\"]+)"?,(\d+\.\d+)?,?"?(\[[^\]]+\]|\[\])"?,"?(\[[^\]]+\]|\[\])"?,(\d{4}-\d{2}-\d{2})?,(\d+),(\d+\.?\d*)?,"?(\[[^\]]+\]|\[\])"?,([^,]+),([^,]+|"[^"]+")?,"?([^\"]+)"?,(\d{1,2}\.\d{1,2}),(\d+)'
-DLIST_REGEX = r'{[^}]+}'
-QUOTES_REGEX = r'(""([^\"]+)"")'
 
 def get_movie_info(line):
-	# Troca aspas duplas duplicadas por aspas simples
-	p = re.compile(QUOTES_REGEX)
-	line = p.sub(r"'\2'", line)
-	# Busca o regex
-	m = re.search(CSV_REGEX, line)
-	if m == None:
-		#print(line)
+	'''
+		input:
+			- line é uma linha da tabela de dados no formato de lista
+		output:
+			- objeto contendo informações de nome, tagline, overview, keywords, e genres do filme
+		
+		cria um objeto filme com os dados da linha da tabela
+	'''
+	try:
+		# Constroi o objeto
+		movie = {}
+		if len(line) > 16:
+			# Nome em inglês
+			movie['name'] = line[17]
+			# Tagline
+			movie['tagline'] = line[16]
+		else:
+			# Nome no idioma original
+			movie['name'] = line[6]
+		# Sumário
+		movie['overview'] = line[7]
+		## Palavras chave
+		movie['keywords'] = []
+		for k in json.loads(line[4]):
+			movie['keywords'].append(k['name'])
+		## Gêneros
+		movie['genres'] = []
+		for g in json.loads(line[1]):
+			movie['genres'].append(g['name'])
+		return movie
+	except:
 		return {}
-	# Constroi o objeto
-	movie = {}
-	# Nome
-	movie['name'] = {'original':m.group(7), 'english':m.group(18)}
-	# Votos
-	movie['votes'] = {'average':m.group(19), 'count':m.group(20)}
-	# Tagline
-	movie['tagline']  = m.group(17)
-	# Sumário
-	movie['overview'] = m.group(8)
-	## Palavras chave
-	#movie['keywords'] = []
-	#n = re.findall(DLIST_REGEX, m.group(5))
-	#for keyword in n:
-	#	movie['keywords'].append(keyword.split('"')[10])
-	## Gêneros
-	#movie['genres'] = []
-	#n = re.findall(DLIST_REGEX, m.group(2))
-	#for genre in n:
-	#	movie['genres'].append(genre.split('"')[10])
-
-	return movie
-
 
 def read_movies(path, display_quantity=False, limit=-1):
+	'''
+		input:
+			- path é o caminho do arquivo de dados no formato .csv
+			- display_quantity é um flag de depuração, que indica se o a chamada deve exibir o número de filmes lidos e a quantidade de filmes que não foram adicionados na lista
+			- limit quando positivo, indica a quantidade máxima de filmes a serem listados, utilize para depuração
+		output:
+			- uma lista de objetos filme, construídos pela função get_movie_info
+		
+		retorna uma uma lista de filmes com as informações relevantes para a análise
+	'''
 	movies = []
 	num_movies = 0
 	with open(path) as f:
 		f.readline() # ignora o cabeçalho
-		for line in f:
+		csvdata = csv.reader(f, delimiter=',')
+		for line in csvdata:
 			num_movies += 1
 			m = get_movie_info(line)
-			if m != {}:
+			# Adiciona apenas se tiver informação
+			if m!= {}:
 				movies.append(m)
+			# Verifica o limite
 			if limit > 0 and num_movies >= limit:
 				break
+	# Exibe informações para fins de depuração
 	if display_quantity:
 		print("Imported: {:d}, Dropped: {:d}".format(len(movies), num_movies - len(movies)))
 	return movies
 
 def save_data(data, path):
+	'''
+		input:
+			- data são os dados a serem salvos
+			- path é o caminho do arquivo
+		
+		salva os dados para um arquivo no formato .json
+	'''
 	with open(path, 'w') as f:
 		json.dump(data, f)
 
